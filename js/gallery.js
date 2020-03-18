92e7631e8329fb19a7081343cb939a2d5a4d2c19
Zhigulskiy <evgeniy1st@users.noreply.github.com>
2020-03-18 23:57:59 +0300

    задаёт стиль для отображения ошибок

--- js/gallery.js
+++ js/gallery.js
@@ -4,7 +4,11 @@
   var listPosts = document.querySelector('.pictures');
 
   var errorHandler = function (message) {
-    console.error(message);
+    var errorNotification = document.createElement('div');
+
+    errorNotification.textContent = message;
+    errorNotification.setAttribute('style', 'background-color: red; font-size: 16px; display: flex; justify-content: center; align-items: center; width: 100%; color: #fff; padding: 10px 0;');
+    document.querySelector('body').insertAdjacentElement('afterbegin', errorNotification);
   };
 
   var renderPosts = function (data) {
