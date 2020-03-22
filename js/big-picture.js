163efb2aa2ad644ee288367bfbec4f813ec9807e
Zhigulskiy <evgeniy1st@users.noreply.github.com>
2020-03-22 17:03:19 +0300

    добавляет кнопку загрузки комментариев

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,9 +1,11 @@
 'use strict';
 
 (function () {
+  var COMMENT_STEP = 5;
   var body = document.querySelector('body');
   var bigPictureContainer = document.querySelector('.big-picture');
   var btnCloseBigPicture = bigPictureContainer.querySelector('.big-picture__cancel');
+  var commentsLoader = bigPictureContainer.querySelector('.comments-loader');
 
 
   var createComment = function (postData, commentIndex) {
@@ -11,34 +13,56 @@
     var commentImg = comment.querySelector('.social__picture');
 
 
-    commentImg.src = postData.comments[commentIndex].avatar;
-    commentImg.alt = postData.comments[commentIndex].name;
-    comment.querySelector('.social__text').textContent = postData.comments[commentIndex].message;
+    commentImg.src = postData[commentIndex].avatar;
+    commentImg.alt = postData[commentIndex].name;
+    comment.querySelector('.social__text').textContent = postData[commentIndex].message;
 
     return comment;
   };
 
   var showBigPicture = function (data) {
     var commentsList = document.querySelector('.social__comments');
+    var totalComments = data.comments;
+    var totalCommentsLength = totalComments.length;
+    var count = 0;
+    var fragment = document.createDocumentFragment();
 
     bigPictureContainer.querySelector('.big-picture__img img').src = data.url;
     bigPictureContainer.querySelector('.likes-count').textContent = data.likes;
-    bigPictureContainer.querySelector('.comments-count').textContent = data.comments.length;
     bigPictureContainer.querySelector('.social__caption').textContent = data.description;
 
-    var fragment = document.createDocumentFragment();
-    for (var i = 0; i < data.comments.length; i++) {
-      fragment.appendChild(createComment(data, i));
-    }
-    commentsList.innerHTML = '';
-    commentsList.appendChild(fragment);
+    var commentsClickHandler = function (first) {
+      var step;
+      if (totalCommentsLength - count - COMMENT_STEP >= 0) {
+        step = COMMENT_STEP;
+      } else {
+        step = totalCommentsLength - count;
+      }
+      for (var i = count; i < count + step; i++) {
+        fragment.appendChild(createComment(totalComments, i));
+      }
+      if (totalCommentsLength >= count + COMMENT_STEP) {
+        count += COMMENT_STEP;
+      } else {
+        count = totalCommentsLength;
+        commentsLoader.removeEventListener('click', commentsClickHandler);
+      }
+      bigPictureContainer.querySelector('.social__comment-count').textContent = count + ' из ' + totalCommentsLength + ' комментариев';
+      if (first === true) {
+        commentsList.innerHTML = '';
+      }
+      commentsList.appendChild(fragment);
+    };
 
-    bigPictureContainer.querySelector('.social__comment-count').classList.add('hidden');
-    bigPictureContainer.querySelector('.comments-loader').classList.add('hidden');
+
     body.classList.add('modal-open');
     bigPictureContainer.classList.remove('hidden');
 
     document.addEventListener('keydown', keydownHandler);
+
+    commentsLoader.addEventListener('click', commentsClickHandler);
+
+    commentsClickHandler(true);
   };
 
   var closeBigPicture = function () {
